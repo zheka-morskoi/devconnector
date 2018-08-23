35430f9cf5f2dd03ea34a2ccadd9d6db014096f8
Evgeniy Morskoy <zhekamorskoi@gmail.com>
2018-08-23 19:12:34 +0300

    redux login & 'set_current_user'

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -20,9 +20,9 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
+  componentDidUpdate(prevProps) {
+    if (prevProps.errors !== this.props.errors) {
+      this.setState({ errors: this.props.errors });
     }
   }
 
